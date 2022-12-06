d85d933a82c231f3ea868c77fe75fd549513a58a
Ivan Vitsin <vanyavitsin@gmail.com>
2022-12-06 08:28:56 +0500

    Initial commit

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,9 @@
+package jm.task.core.jdbc;
+
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
+
+public class Main {
+    public static void main(MysqlxDatatypes.Scalar.String[] args) {
+        // реализуйте алгоритм здесь
+    }
+}
