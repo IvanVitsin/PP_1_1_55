5e068d247ee33edf02f124e9919582a19348ec39
Ivan Vitsin <vanyavitsin@gmail.com>
2022-12-08 11:26:48 +0500

    default constructor was removed

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -12,10 +12,6 @@ public class UserDaoJDBCImpl implements UserDao {
 
     private final Connection connection = Util.getConnection();
 
-    public UserDaoJDBCImpl() {
-
-    }
-
     public void createUsersTable() {
 
         try (Statement statement = connection.createStatement()) {
